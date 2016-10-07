1eb9e0d33929ecfb6a6b02037d99ee6aeabfacd8
gustavekd <gustav.ekelund99@gmail.com>
2016-10-07 12:22:14 +0200

    ds

--- string formatering/Program.cs
+++ string formatering/Program.cs
@@ -9,7 +9,35 @@ namespace string_formatering
     class Program
     {
         static void Main(string[] args)
+        //övning1
+
         {
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt.ToString("D"));
+
+            //övning2
+            double cirkel = Math.PI * 4 * 4;
+            Console.WriteLine(cirkel.ToString("F4"));
+
+            //övning3
+            int tal = 1000000000;
+            Console.WriteLine(tal.ToString("0,0,0,"));
+
+            //övning4
+            double value = 123.00;
+            Console.WriteLine(value.ToString("\\#\\#\\# ##0 kr och \\0\\0 öre \\#\\#\\#"));
+
+            //övning5
+            Console.WriteLine("mata in personnummer utan mellanslag:");
+            string personnr = Console.ReadLine();
+            decimal pntal = decimal.Parse(personnr);
+            Console.WriteLine(pntal.ToString("yyyy mm dd - kkkk"));
         }
+        
+        
+
+        
+        
+
     }
 }
